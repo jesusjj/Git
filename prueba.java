34435bd156425a1e6081a3ed72e55776b430ba0a
jesus jj <jesusjj@example.com>
2016-10-26 10:52:01 +0200

    Añadida la resta

--- prueba.java
+++ prueba.java
@@ -1,9 +1,12 @@
 
 public class prueba {
-	public static void main (String [] args){
+	public static void main(String[] args) {
 		int a = 3;
 		int b = 9;
 		int resultado = a + b;
-		System.out.println("El resultado es"+resultado);
+		int resta = a-b;
+		
+		System.out.println("El resultado es" + resultado);
+		System.out.println("El resultado es" + resta);
 	}
 }
